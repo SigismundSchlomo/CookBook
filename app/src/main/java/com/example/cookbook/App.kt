d474cabc833935a76c62d97791d55eff2dd41d3a
SigismundSchlomo <kuzenstein23@gmail.com>
2020-09-03 13:58:25 +0300

    Recipe repository injection implemented

--- app/src/main/java/com/example/cookbook/App.kt
+++ app/src/main/java/com/example/cookbook/App.kt
@@ -1,9 +1,15 @@
 package com.example.cookbook
 
 import android.app.Application
+import com.example.cookbook.di.AppComponent
+import com.example.cookbook.di.DaggerAppComponent
 import timber.log.Timber
 
 open class App() : Application() {
+
+    val appComponent: AppComponent by lazy {
+        DaggerAppComponent.create()
+    }
 
     override fun onCreate() {
         super.onCreate()
--- app/src/main/java/com/example/cookbook/di/AppComponent.kt
+++ app/src/main/java/com/example/cookbook/di/AppComponent.kt
@@ -1,9 +1,13 @@
 package com.example.cookbook.di
 
+import com.example.cookbook.presentation.MainActivity
 import dagger.Component
+import javax.inject.Singleton
 
+@Singleton
 @Component(modules = [NetworkModule::class])
 interface AppComponent {
 
+    fun inject(activity: MainActivity)
 
 }
