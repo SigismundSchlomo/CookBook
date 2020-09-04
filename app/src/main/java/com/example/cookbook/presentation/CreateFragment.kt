cec24d24b0785413ae1f21dc8ab145649fd461cb
SigismundSchlomo <kuzenstein23@gmail.com>
2020-09-04 16:23:12 +0300

    Create Recipe fragment created

---
+++ app/src/main/java/com/example/cookbook/presentation/CreateFragment.kt
@@ -0,0 +1,25 @@
+package com.example.cookbook.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.cookbook.R
+
+class CreateFragment : Fragment() {
+
+    companion object {
+        fun newInstance(): CreateFragment {
+            return CreateFragment()
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_create_recipe, container, false)
+    }
+}
