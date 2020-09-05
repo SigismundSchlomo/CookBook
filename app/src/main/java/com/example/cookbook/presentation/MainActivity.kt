f46341d6c7ae3513a5ae98d990e21bbdf1551d0c
SigismundSchlomo <kuzenstein23@gmail.com>
2020-09-05 11:47:19 +0300

    State machine deleted

--- app/src/main/java/com/example/cookbook/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cookbook/presentation/MainActivity.kt
@@ -6,12 +6,8 @@ import androidx.fragment.app.commit
 import com.example.cookbook.App
 import com.example.cookbook.R
 import timber.log.Timber
-import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
-
-    @Inject
-    lateinit var stateMachine: MainActivityStateMachine
 
     override fun onCreate(savedInstanceState: Bundle?) {
         (application as App).appComponent.inject(this)
@@ -19,13 +15,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Timber.d("Main activity onCreate")
 
-        val nextScreen = when (stateMachine.currentState) {
-            MainActivityStateMachine.State.RECIPE_LIST -> ListFragment.newInstance()
-            MainActivityStateMachine.State.CREATE_RECIPE -> CreateFragment.newInstance()
-        }
-
         supportFragmentManager.commit {
-            replace(R.id.main_fragment_container, nextScreen)
+            replace(R.id.main_fragment_container, ListFragment.newInstance())
         }
 
     }
