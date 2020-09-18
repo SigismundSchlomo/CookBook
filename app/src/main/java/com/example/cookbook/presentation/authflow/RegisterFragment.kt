e14fc0ccf863af144ad0e78d4b3b381a809542cd
SigismundSchlomo <kuzenstein23@gmail.com>
2020-09-18 19:38:43 +0300

    Register fragment updated

--- app/src/main/java/com/example/cookbook/presentation/authflow/RegisterFragment.kt
+++ app/src/main/java/com/example/cookbook/presentation/authflow/RegisterFragment.kt
@@ -8,12 +8,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
 import com.example.cookbook.App
 import com.example.cookbook.R
 import com.example.cookbook.di.injectViewModel
+import com.example.cookbook.domain.models.isValid
+import com.example.cookbook.presentation.ErrorMessage
 import com.example.cookbook.presentation.mainflow.MainActivity
 import com.example.cookbook.utils.ConnectivityManagerWrapper
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_register.*
+import java.util.*
 
 import javax.inject.Inject
 
@@ -58,5 +63,28 @@ class RegisterFragment : Fragment() {
             startActivity(intent)
         }
 
+        viewModel.userLiveData.observe(viewLifecycleOwner) { user ->
+            val currentDate = Calendar.getInstance().time
+            if (user.token.isValid(currentDate)) {
+                val intent = Intent(activity, MainActivity::class.java)
+                startActivity(intent)
+                activity?.finish()
+            }
+        }
+
+        viewModel.errorMessage.observe(viewLifecycleOwner) { errorMessage ->
+            val messageResource = when (errorMessage) {
+                ErrorMessage.SERVICE_UNAVAILABLE -> R.string.service_unavailable
+                ErrorMessage.UNKNOWN_ERROR -> R.string.unknown_error
+                ErrorMessage.DATA_FROM_DATABASE -> R.string.data_from_database
+            }
+            showErrorMessage(messageResource)
+        }
+
     }
+
+    private fun showErrorMessage(stringResource: Int) {
+        Snackbar.make(requireView(), stringResource, Snackbar.LENGTH_SHORT).show()
+    }
+
 }
